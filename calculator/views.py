433988810183cdf6644ab28a8527a67a4983acde
Huy Hoang Nguyen <nguyenhhoang.hhn@gmail.com>
2026-04-05 12:38:02 -0400

    Add: Connect FE & BE

--- calculator/views.py
+++ calculator/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .subnet_calc import calculate
+from .subnet_calc import calculate, vlsm_allocate
 
 def home(request):
     context = {}
@@ -7,6 +7,7 @@ def home(request):
     if request.method == "POST":
         ip_address = request.POST.get("ip_address", "").strip()
         cidr_input = request.POST.get("cidr", "").strip()
+        hosts_input = request.POST.get("hosts", "").strip()
 
         try:
             cidr = int(cidr_input)
@@ -14,16 +15,23 @@ def home(request):
             if cidr < 0 or cidr > 32:
                 raise ValueError("CIDR must be between 0 and 32.")
 
-            result = calculate(ip_address, cidr)
+            if hosts_input:
+                host_list = [int(x.strip()) for x in hosts_input.split(",")]
 
+                vlsm_result = vlsm_allocate(ip_address, cidr, host_list)
+                context["vlsm"] = vlsm_result
+
+
+            result = calculate(ip_address, cidr)
             context["result"] = result
+
             context["ip_address"] = ip_address
             context["cidr"] = cidr
+            context["hosts"] = hosts_input
 
         except ValueError as e:
             context["error"] = str(e)
         except Exception:
-            context["error"] = "Invalid input. Please enter a valid IPv4 address and CIDR."
+            context["error"] = "Invalid input. Check IP, CIDR, or hosts."
 
     return render(request, "calculator/home.html", context)
-
